9a91f2cd8866cdaf9f13bd676d930ba32cf97417
spacejam909 <aliya.279n@gmail.com>
2024-10-28 23:47:10 -0500

    Initial Commit

---
+++ DeeemoooApp/DeeemoooAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeeemoooAppApp.swift
+//  DeeemoooApp
+//
+//  Created by Aliya Omorova on 10/28/24.
+//
+
+import SwiftUI
+
+@main
+struct DeeemoooAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DeeemoooAppTests/DeeemoooAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  DeeemoooAppTests.swift
+//  DeeemoooAppTests
+//
+//  Created by Aliya Omorova on 10/28/24.
+//
+
+import Testing
+@testable import DeeemoooApp
+
+struct DeeemoooAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
